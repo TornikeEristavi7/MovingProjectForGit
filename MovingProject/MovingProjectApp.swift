3792e3163ef2a4b3738b50019310e2644ce4635d
Tornike <tornikeeristavi43@gmail.com>
2024-10-17 12:55:33 +0400

    Initial Commit

---
+++ MovingProject/MovingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingProjectApp.swift
+//  MovingProject
+//
+//  Created by Tornike Eristavi on 17.10.24.
+//
+
+import SwiftUI
+
+@main
+struct MovingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
